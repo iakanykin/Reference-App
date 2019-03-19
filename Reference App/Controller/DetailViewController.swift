b51fb6ae5d6ba47ec4a1d8f4776c4fa4dc9dd9f1
Denis Bystruev <dbystruev@me.com>
2019-03-19 17:01:04 +0300

    Finished the project

---
+++ Reference App/Controller/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  Reference App
+//
+//  Created by Denis Bystruev on 19/03/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    var model: ReferenceModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        imageView.image = model.image
+        titleLabel.text = model.title
+        detailLabel.text = model.detail
+    }
+}
